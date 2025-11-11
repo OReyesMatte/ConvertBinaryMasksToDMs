c9e37eda0dfa572465c2f7bd9fa439be6d80a1de
OReyesMatte <reyesmatte@proton.me>
2025-11-11 16:56:18 +0100

    Added functions to convert mask to skeleton for then computing the distance matrix on the skeleton spline

--- helpers.py
+++ helpers.py
@@ -7,7 +7,7 @@ import skimage
 import numpy as np
 from scipy.spatial import distance_matrix
 from scipy.interpolate import splprep, splev
-
+from cv2 import findNonZero
 ###############################################################################
 
 def download(url, fname = None):
@@ -175,7 +175,7 @@ def find_longest_contour(mask, normalise_coord=False):
   # return the contour as a pair of lists of x and y coordinates
   return np.column_stack([x, y])
 
-def contour_spline_resample(contour, n_samples, sparsity=1):
+def contour_spline_resample(contour, n_samples, sparsity=1, per=True):
   """
   Return a resampled spline interpolation of a provided contour
 
@@ -205,10 +205,244 @@ def contour_spline_resample(contour, n_samples, sparsity=1):
   tck, u = splprep( sparse_contour.T # reshaped contour
                   , s = 0 # XXX
                   , k = min(3, len(sparse_contour) - 1)
-                  , per = True # closed contour (periodic spline)
+                  , per = per # closed contour (periodic spline)
                   )
   # how many times to sample the spline
   # last parameter is how dense is our spline, how many points.
   new_u = np.linspace(u.min(), u.max(), n_samples)
   # evaluate and return the sampled spline
   return np.column_stack(splev(new_u, tck))
+
+#######################
+
+# The functions euclidean_distance, compute_edges, build_graph, calculate_total_distance, DFS, and PathFinder are copied from the MEDUSSA repository https://github.com/OReyesMatte/MEDUSSA
+
+def euclidean_distance(p1:tuple,p2:tuple)->float:
+    """Function to calculate the Euclidean distance between two points in a two-dimensional space
+
+    Args:
+        p1(tuple): coordinates (x,y) of the first point
+        p2(tuple): coordinates (x,y) of the second point
+
+    Returns: 
+        float: distance value between the two points
+    """
+
+    return np.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
+
+def compute_edges(points:list,threshold:float)->list:
+    
+    """Convert a list of spatial coordinates to an edge list, where each point is considered to be the edge of a specific point if it's below a certain distance.
+    If it is only one point, then it won't go through, as it wouldn't be a rod-shaped cell
+
+    Args:
+        points(list): list of points coordinates (x,y)
+        threshold(float): minimum distance for two points to be considered edges
+
+    Returns: 
+        edges: list of candidate edges for each point
+    """
+    
+    edges = []
+
+    if len(points) == 1:
+        return print("Only one point, cannot be converted to graph")
+
+    else:
+
+        for i in range(len(points)):
+            for j in range(i+1, len(points)):
+
+                p1 = points[i]
+                p2 = points[j]
+                distance = euclidean_distance(p1,p2)
+                if distance <= threshold:
+                    edges.append((p1,p2))
+
+        return edges
+
+## Build graph as list
+def build_graph(points:list,edges:list)->dict:
+
+    """Convert a list of points and edges into a graph
+    
+    Args:
+        points(list): list of points coordinates (x,y)
+        threshold(float): minimum distance for two points to be considered edges
+
+    Returns: 
+        graph(dict): graph representation of the skeleton, where lists the edges of each point in the skeleton
+    """
+
+    graph = {point: [] for point in points}
+    
+    for p1,p2 in edges:
+        graph[p1].append(p2)
+        graph[p2].append(p1)
+
+    return graph
+
+def calculate_total_distance(path:list)->float:
+
+    """Calculate the total distance as the seum of euclidean distances between two consecutive points
+    
+    Args:
+        path(list): list of points in (x,y) coordinate
+
+    Return:
+        total_distance(float): total calculated distance as a single float number
+    """
+
+    total_distance = 0
+
+    for i in range(len(path) - 1):
+        total_distance += euclidean_distance(path[i],path[i+1])
+    return total_distance
+
+### Depth First Search
+def DFS(graph, current, visited, path, longest_path, longest_distance):
+
+    """Run a Depth First Search algorithm 
+    """
+
+    visited.add(current)
+    path.append(current)
+
+    ## Update path
+    if len(path) > len(longest_path):
+        longest_path[:] = path[:]
+        longest_distance[0] = calculate_total_distance(path)
+
+    for neighbor in graph[current]:
+        if neighbor not in visited:
+            DFS(graph, neighbor, visited, path, longest_path, longest_distance)
+
+    visited.remove(current)
+    path.pop
+
+### Check for multible possible paths
+def PathFinder(path:list,points:list,threshold:float)->list:
+
+    """ It can happen that DFS computes multiple possible paths, leaving to wrong measurements. 
+    For a candidate longest path, find if, indeed, is it just one path or multiple, as the latter can happen with branched skeletons
+    If two successive points in the path are more than one threshold away (typically, the euclidean distance between two diagonal points), the path is assumed to split there into multiple
+    If the path is too short (less than 10 pixels, or less than 0.65 µm of length), it's discarded.
+
+    Args:
+        path(list): 
+        points(list): list of points coordinates (x,y)
+        threshold(float): minimum distance for two points to be considered edges
+
+    Returns: 
+        paths(list): computed individual paths
+    """
+
+    paths = []
+
+    j = 0
+    
+    for i in range(len(path)-1):
+
+        if euclidean_distance(path[i],path[i+1]) > threshold:
+            
+            subpath = path[j:(i+1)]
+            paths.append(subpath)
+            j = i+1
+
+    for i,p in enumerate(paths):
+
+        if len(p) < 10:
+            del paths[i]
+
+    return paths
+
+
+##############################
+
+def count_edges(points:np.array,threshold:float=np.sqrt(2))->int:
+
+    n_edges = 0
+
+    for i in range(len(points)):
+    
+        neighbors = 0
+    
+        for j in range(len(points)):
+    
+            distance = euclidean_distance(points[i],points[j])
+    
+            if distance > 0 and distance <= threshold: neighbors +=1 
+    
+            if neighbors > 2: break
+    
+        if neighbors == 1: n_edges += 1
+
+    return n_edges
+    
+
+def mask2sampledSkel(mask:np.array, n_samples:int=64, resample_sparsity:int=1, closed:bool=False,threshold=np.sqrt(2)):
+    """
+    Return a resampled spline interpolation of the skeleton of a mask
+    
+    Parameters
+    ----------
+    mask : 2-d numpy array
+    A binary mask of a single cell
+    n_samples : int
+    The number of points to sample on the spline
+    sparsity : int, default=1
+    The distance (in number of gaps) to the next point to consider in the
+    original contour (i.e. whether to consider every point, every other point,
+    every 3 points... One would consider non-1 sparsity to avoid artifacts due
+    to high point count contours over low pixel resolution images, with contours
+    effectively curving around individual pixel edges)
+    closed : bool, default = False
+    A necessary flag to return a resampled skeleton, else it will close it in undesired ways
+    
+    Returns
+    -------
+    2-d numpy array
+    The spline-resampled skeleton with n_samples points as a sequence of x, y
+    coordinates
+    """
+    
+    skel = skeletonize(img)
+    points = np.squeeze((cv2.findNonZero(np.uint8(skel))))
+    points = np.array([tuple(xy) for xy in points])
+
+    ### First, check that the skeleton is not branched
+    if count_edges(skel,threshold) == 2:
+    
+        resampled = contour_spline_resample(points,n_samples,per=closed)
+        
+        y,x = resampled[:,0],resampled[:,1]
+    
+        return np.stack([x,y],axis=1)
+
+    ### If branched, compute candidate skeletons from the branched one, keep only the ones that cover at least 75% of the total skeleton
+    else: 
+        #### Compute edges and build graph
+        edges = compute_edges(points=points,threshold=threshold)
+        graph = build_graph(points=points,edges=edges)
+
+        #### Calculate the longest path of the skeleton
+        longest_path = []
+        longest_distance = [0]
+
+        for point in points:
+            visited = set()
+            DFS(graph,point,visited,[],longest_path,longest_distance)            
+
+        paths = PathFinder(path=longest_path,points=points,threshold=threshold)
+
+        resampled = [contour_spline_resample(p,n_samples,per=closed) for p in paths]
+
+        resampled_ = []
+
+            for r in resampled:
+                
+                y,x = r[:,0],r[:,1]
+                resampled_.append(np.stack([x,y],axis=1))
+                
+        return resampled_
+    
